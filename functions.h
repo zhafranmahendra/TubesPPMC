b6eab2fc247dce309576977b61b955154147385b
Joshtein <60211084+Joshtein@users.noreply.github.com>
2020-04-13 04:56:34 +0700

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,15 @@
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020  
+* MODUL 9 – TUGAS BESAR  
+* Kelompok   : 5  
+* Hari dan Tanggal : Senin, 13 April 2018 
+* Asisten (NIM)  : Meinanto Tri Yuriawan (13217021) 
+* Nama File   : functions.h  
+* Deskripsi   : Implementasi fungsi Tick dan count_neighbor  */ 
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <time.h> 
+
+int count_neighbor(int jumlah_baris, int jumlah_kolom, int posisi_baris, int posisi_kolom, char *matriks){
