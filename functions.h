b5ba32318f6beef0f6cc46e35a5be1eceed784b6
silmy750 <63683072+silmy750@users.noreply.github.com>
2020-04-16 16:00:04 +0700

    Update functions.h

--- functions.h
+++ functions.h
@@ -14,6 +14,29 @@
 #include <conio.h>
 #include <time.h> 
 
+void Matriks_Input(int jumlah_baris, int jumlah_kolom, char *MatriksInput, FILE *file){
+	int i,j;
+	char buf[jumlah_kolom];
+	for(i=0;i<jumlah_baris;i++){
+		fgets(buf,(jumlah_kolom+2),file);
+		for(j=0;j<jumlah_kolom;j++){
+			*((MatriksInput+(i*jumlah_kolom))+j) = buf[j];
+		};
+	};
+};
+
+void printMatriks(int jumlah_baris, int jumlah_kolom, char *matriks){
+	int i,j;
+	printf("\nTampilan Matriks\n\n");
+	for(i=0;i<jumlah_baris;i++){
+		for(j=0;j<jumlah_kolom;j++){
+			printf("%c ",*((matriks+(i*jumlah_kolom))+j));
+		};
+		printf("\n");
+	};
+};
+
+
 //fungsi untuk menghitung jumlah tetangga yang hidup pada suatu sel
 int count_neighbor (int jumlah_baris, int jumlah_kolom, int posisi_baris, int posisi_kolom, char *matriks){
  /*
